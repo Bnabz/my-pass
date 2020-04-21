ce1e15baa7ba748d15e79ded5b6c1b8e741d92f6
Nabz <bnnabiswa@gmail.com>
2020-04-21 05:10:54 +0300

    pass init test

--- password.py
+++ password.py
@@ -1,11 +1,11 @@
 import pyperclip
 class User:
     user_list = []
-    # def __init__(self,first_name, last_name, user_name, password):
-    #     self.first_name = first_name
-    #     self.last_name = last_name
-    #     self.user_name = user_name
-    #     self.password = password
+    def __init__(self,first_name, last_name, user_name, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
 
  
 
