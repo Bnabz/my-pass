f9274d79f034f4b9f004c4ec2cf60fa0f3166b7f
Nabz <bnnabiswa@gmail.com>
2020-04-21 05:10:03 +0300

    ran init test, failed

--- password_test.py
+++ password_test.py
@@ -6,6 +6,12 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Brian", "Nabiswa", "Bnabz", "mypass")
 
+    def test_init(self):
+        self.assertEqual(self.new_user.first_name, "Brian")
+        self.assertEqual(self.new_user.last_name, "Nabiswa")
+        self.assertEqual(self.new_user.user_name, "Bnabz")
+        self.assertEqual(self.new_user.password, "mypass")
+
 
 
 if __name__ == '__main__':
