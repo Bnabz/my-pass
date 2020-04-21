ff0c55682fb5c33cc6ce2b882891b60f415cf6ff
Nabz <bnnabiswa@gmail.com>
2020-04-21 05:15:17 +0300

    fail save user test

--- password_test.py
+++ password_test.py
@@ -12,6 +12,9 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.user_name, "Bnabz")
         self.assertEqual(self.new_user.password, "mypass")
 
+    def test_save_user(self):
+        self.new_user.save_user() #saving the new user_list
+        self.assertEqual(len(User.user_list),1)
 
 
 if __name__ == '__main__':
