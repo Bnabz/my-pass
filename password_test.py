3c3bd80c567a04d72d4eeac378d1cbbd9150100d
Nabz <bnnabiswa@gmail.com>
2020-04-21 05:39:15 +0300

    fail delete user test

--- password_test.py
+++ password_test.py
@@ -26,6 +26,13 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(len(User.user_list),2)
 
+    def test_delete_user(self):
+        self.new_user.save_user()
+        test_user = User("mullah", "Mwah", "comic", "choma")
+        test_user.save_user()
+        self.new_user.delete_user() 
+        self.assertEqual(len(User.user_list),1)
+
 
 if __name__ == '__main__':
     unittest.main()
